a585ac5d8e4ae649915d5ad5ced0d5a134682084
Dorel Barbu <dobarbu@bitdefender.biz>
2022-01-14 13:30:10 +0200

    First commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+@Controller('users')
+export class UsersController {
+  constructor(
+    private usersService: UsersService,
+  ) {}
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto) {
+    return body;
+  }
+}
